0ea34b139e43366c565545d4f3d3f31d862a5cf7
rawlinsg <rawlins@triply.co>
2025-06-26 00:18:15 +0300

    Astro-Vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,15 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 import tailwind from "@astrojs/tailwind";
+import vercel from '@astrojs/vercel';
 
 const isProd = process.env.NODE_ENV === "production";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind()],
-  site: "https://creativetimofficial.github.io",
+  output: "server",
+  adapter: vercel(),
+  site: "https://www.grupchat.info",
   base: "/",
 });
